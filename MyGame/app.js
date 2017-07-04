9049c90e101bc8d71e322237cff07e09e964d419
StefanoCvitanich <stefanojc@gmail.com>
2017-07-03 22:24:15 -0300

    Input y Atlas
    Hay que cortar el atlas
    Hay que crear mas objetos tipo tecla

--- MyGame/app.js
+++ MyGame/app.js
@@ -1,17 +1,21 @@
 var MyGame = (function () {
     function MyGame() {
+        this.moveRight = new Phaser.Key(this.game, Phaser.KeyCode.D);
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create, update: this.update });
     }
     MyGame.prototype.preload = function () {
         this.game.load.image('logo', 'phaser2.png');
+        //this.game.load.image('brekOut_Sprite_sheet', 'breakout_sprites.png');
     };
     MyGame.prototype.create = function () {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
         logo.anchor.setTo(0.5, 0.5);
         logo.scale.setTo(0.2, 0.2);
         this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+        //var player: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'brekOut_Sprite_sheet');
     };
     MyGame.prototype.update = function () {
+        if () { }
     };
     return MyGame;
 }());
--- MyGame/app.ts
+++ MyGame/app.ts
@@ -6,8 +6,11 @@
 
     game: Phaser.Game;
 
+    moveRight: Phaser.Key = new Phaser.Key(this.game, Phaser.KeyCode.D);
+
     preload() {
         this.game.load.image('logo', 'phaser2.png');
+        //this.game.load.image('brekOut_Sprite_sheet', 'breakout_sprites.png');
     }
 
     create() {
@@ -17,11 +20,15 @@
         logo.scale.setTo(0.2, 0.2);
 
         this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+
+        //var player: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'brekOut_Sprite_sheet');
+
     }
 
     update() {
 
-
+        if ()
+        { }
     }
 
 }
